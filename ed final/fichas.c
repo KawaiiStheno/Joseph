8e178ac980d8c250b2aea7325919e95fb663f65a
KawaiiStheno <47184562+KawaiiStheno@users.noreply.github.com>
2019-07-07 10:58:44 -0300

    Update fichas.c

--- ed final/fichas.c
+++ ed final/fichas.c
@@ -40,44 +40,23 @@ int lista_vazia(Lista* li){
 	return 0;
 }
 
-int insere_ordenada(Lista* li, struct entrada al){
-	if(li == NULL){
-		return 0;
-	}
-	Elem *no = (Elem*)malloc(sizeof(Elem));
-	if(no == NULL){
-		return 0;
-	}
-	no->dados = al;
-	if(lista_vazia(li)){
-		no->prox=(*li);
-		*li = no;
-		return 1;
-	}else{
-		Elem *ant, *atual = *li;
-		while(atual != NULL && atual->dados.id <al.id){
-			ant = atual;
-			atual = atual->prox;
-		}
-		if(atual == *li){
-			no->prox = (*li);
-			*li = no;
-		}else{
-			no->prox = ant->prox;
-			ant->prox = no;
-		}
-		return 1;
-	}
-}
-void imprime(Lista* li){
+void imprime(Lista* li, int auxi){
 	if(li==NULL||(*li)==NULL){
-		printf("A lista esta vazia!\n");
+		printf("A estrutura escolhida nao possui elementos.\n");
 	}else{
 		Elem *aux = *li;
-		int cont = 0;
+		int cont = 0, bloco=1;
 		while(aux != NULL){
+			if(auxi==2){
+				if(cont==0||cont % 10==0){
+					printf("\t\t\tBloco %d\n",bloco);
+					printf("Hash Anterior: %x\n",(aux->dados).prevHash);
+					printf("Hash: %x\n\n",(aux->dados).hash);
+					bloco++;
+				}
+			}
 			cont++;
-			printf("\t\t\tJogador %d\n",cont);
+			printf("Jogador %d\n",cont);
 			printf("Id do Jogador: %d\n",(aux->dados).id);
 			printf("Nome do Jogador: %s\n",(aux->dados).nome);
 			printf("Nome do Personagem: %s\n",(aux->dados).player);
@@ -87,20 +66,31 @@ void imprime(Lista* li){
 			printf("Destreza: %d\n",(aux->dados).des);
 			printf("Inteligencia: %d\n",(aux->dados).inte);
 			printf("Sabedoria: %d\n",(aux->dados).sab);
-			printf("Carisma: %d\n",(aux->dados).car);
-			
+			printf("Carisma: %d\n\n",(aux->dados).car);
 			aux = aux->prox;
 		}
 	}
 }
 
-int remove_inicio(Lista* li){
-	if(li==NULL||(*li)==NULL){
+int insere_final(Lista* li, struct entrada al){
+	if(li == NULL){
 		return 0;
 	}
-	Elem *no = *li;
-	*li = no->prox;
-	free(no);
+	Elem *no = (Elem*)malloc(sizeof(Elem));
+	if(no == NULL){
+		return 0;
+	}
+	no->dados = al;
+	no->prox = NULL;
+	if((*li)==NULL){
+		*li = no;
+	}else{
+		Elem *aux = *li;
+		while(aux->prox != NULL){
+			aux = aux->prox;
+		}
+		aux->prox = no;
+	}
 	return 1;
 }
 
@@ -122,23 +112,77 @@ int remove_final(Lista* li){
 	return 1;
 }
 
-int remove_qlqr(Lista* li, int id){
-	if(li==NULL){
-		return 0;
+int hash(char data[]){
+	int hash = 0;
+	int cont;
+	for(cont=0;cont<strlen(data);cont++){
+		hash = hash ^ data[cont];
+		hash = hash*0x123456789;
 	}
-	Elem *ant, *no = *li;
-	while(no!=NULL && no->dados.id!=id){
-		ant = no;
-		no=no->prox;
-	}
-	if(no==NULL){
-		return -1;
-	}
-	if(no == *li){
-		*li = no->prox;
-	}else{
-		ant->prox = no->prox;
-	}
-	free(no);
-	return 1;
+	return hash;
 }
+
+int mine_block(char data[]){
+	int nonce = 0;
+	int hash1;
+
+	char dataToHash[256];
+
+	while(1){
+		sprintf(dataToHash,"%s%d",data,nonce);
+		hash1 = hash(dataToHash);
+
+		if((hash1 & 0xff) == 0xAA){
+			return nonce;
+		}
+		nonce++;
+	}
+	return nonce;
+}
+
+int recebe(struct entrada *al, int cont, int flag){
+	struct entrada al2;
+	char converte[250];
+
+	printf("Digite seu ID: ");
+	scanf("%d",&al2.id);
+	getchar();
+    printf("Digite seu nome: ");
+	scanf("%[^\n]",al2.nome);
+	getchar();
+    printf("Digite o nome do seu personagem: ");
+    scanf("%[^\n]",al2.player);
+    getchar();
+    printf("Digite sua raca: ");
+    scanf("%[^\n]",al2.raca);
+    printf("Forca: ");
+    scanf("%d",&al2.fo);
+    printf("Constituicao: ");
+    scanf("%d",&al2.cons);
+    printf("Destreza: ");
+    scanf("%d",&al2.des);
+    printf("Inteligencia: ");
+    scanf("%d",&al2.inte);
+    printf("Sabedoria: ");
+    scanf("%d",&al2.sab);
+    printf("Carisma: ");
+    scanf("%d",&al2.car);
+    if(cont>0){
+    	al2.prevHash = flag;
+		sprintf(converte,"%s%d",al2.nome,al2.prevHash);
+    	al2.nonce = mine_block(converte);
+    	sprintf(converte,"%s%d",converte,al2.nonce);
+    	al2.hash = hash(converte);
+    	flag = al2.hash;
+    }else{
+	   	al2.prevHash = 0;
+    	sprintf(converte,"%s%d",al2.nome,al2.prevHash);
+    	al2.nonce = mine_block(converte);
+    	sprintf(converte,"%s%d",converte,al2.nonce);
+    	al2.hash = hash(converte);
+		flag = al2.hash;
+	}
+    *al = al2;
+    return flag;
+}
+
