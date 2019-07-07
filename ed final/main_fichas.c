110290c5a178b10064fc45dd2ed8e35b9a31e975
KawaiiStheno <47184562+KawaiiStheno@users.noreply.github.com>
2019-07-07 10:56:35 -0300

    Update main_fichas.c

--- ed final/main_fichas.c
+++ ed final/main_fichas.c
@@ -1,117 +1,114 @@
 #include "fichas.h"
-#ifdef _WIN32
-#define CLEAR "cls"
-#else
-#define CLEAR "clear"
-#endif
 
 int main(){
-	int x, aux, aux2;
-	Lista *li;
-	struct entrada al;
+	int x, aux, cont=0, flag, cont2, fhash=0, qtd, cont3;
+	Lista *li, *buffer;
+	struct entrada al[MAX];
 	do{
 		printf("1.Criar lista.\n");
 		printf("2.Verificar se a lista eh vazia.\n");
 		printf("3.Liberar a lista.\n");
 		printf("4.Inserir dados de jogador.\n");
-		printf("5.Remover ficha no inicio da lista.\n");
-		printf("6.Remover ficha no final da lista.\n");
-		printf("7.Remover ficha especifica.\n");
-		printf("8.Verificar a quantidade de jogadores.\n");
-		printf("9.Imprimir fichas.\n");
-		printf("10.Finalizar o programa.\n");
+		printf("5.Remover a ultima ficha inserida.\n");
+		printf("6.Verificar a quantidade de jogadores.\n");
+		printf("7.Imprimir fichas.\n");
+		printf("8.Finalizar o programa.\n");
 		printf("Digite sua escolha: ");
 		scanf("%d",&aux);
 		system(CLEAR);
 		switch(aux){
 			case 1:
-			li = cria_lista();
+				li = cria_lista();
+				buffer = cria_lista();
 			break;
 			case 2:
-			x = lista_vazia(li);
-			if(x==0){
-				printf("A lista nao esta vazia!\n\n");
-			}else{
-				printf("A lista esta vazia!\n\n");
-			}
-			break;
-			case 3:
-			libera_lista(li);
-			break;
-			case 4:
-				printf("Digite seu ID: ");
-				scanf("%d",&al.id);
-				getchar();
-			    printf("Digite seu nome: ");
-			    scanf("%[^\n]",al.nome);
-			    getchar();
-			    printf("Digite o nome do seu personagem: ");
-			    scanf("%[^\n]",al.player);
-			    getchar();
-			    printf("Digite sua raca: ");
-			    scanf("%[^\n]",al.raca);
-			    printf("Forca: ");
-			    scanf("%d",&al.fo);
-			    printf("Constituicao: ");
-			    scanf("%d",&al.cons);
-			    printf("Destreza: ");
-			    scanf("%d",&al.des);
-			    printf("Inteligencia: ");
-			    scanf("%d",&al.inte);
-			    printf("Sabedoria: ");
-			    scanf("%d",&al.sab);
-			    printf("Carisma: ");
-			    scanf("%d",&al.car);
-				x = insere_ordenada(li, al);
-				system(CLEAR);
-			if(x==0){
-				printf("Erro!\n");
-			}else{
-				printf("Ficha inserida com sucesso!\n\n");
-			}
-			break;
-			case 5:
-			x = remove_inicio(li);
-			if(x==0){
-				printf("Erro!\n");
-			}else{
-				printf("Ficha removida com sucesso!\n\n");
-			}
-			break;
-			case 6:
-			x = remove_final(li);
-			if(x==0){
-				printf("Erro!\n");
-			}else{
-				printf("Ficha removida com sucesso!\n\n");
-			}
-			break;
-			case 7:
-				printf("Digite o Id do jogador que deseja remover: ");
-				scanf("%d",&aux2);
-				x = remove_qlqr(li, aux2);
+				x = lista_vazia(li);
 				if(x==0){
-					printf("Erro!\n");
-				}else if(x==-1){
-					printf("Jogador nao encontrado!\n\n");
+					printf("A lista nao esta vazia!\n\n");
 				}else{
-					printf("Jogador removido com sucesso!\n\n");
+					printf("A lista esta vazia!\n\n");
 				}
 			break;
-			case 8:
-			x = tamanho_lista(li);
-			if(x==-1){
-				printf("Erro!\n");
-			}else{
-				printf("A quantidade de jogadores eh:%d\n\n",x);
-			}
+			case 3:
+				libera_lista(li);
+				libera_lista(buffer);
 			break;
-			case 9:
-				imprime(li);
-				printf("\n\n");
+			case 4:
+				printf("Digite quantas fichas deseja inserir: ");
+				scanf("%d",&qtd);
+				system(CLEAR);
+				for(cont3=0;cont3<qtd;cont3++){
+					flag = recebe(&al[cont],cont,flag);
+					x = insere_final(buffer, al[cont]);
+					cont++;
+					system(CLEAR);
+					if(cont==10){
+						for(cont2=0;cont2<MAX;cont2++){
+							al[cont2].prevHash = fhash;
+						}
+						for(cont2=0;cont2<MAX;cont2++){
+							fhash = al[cont2].hash + fhash;
+						}
+						for(cont2=0;cont2<MAX;cont2++){
+							al[cont2].hash = fhash;
+							x = insere_final(li, al[cont2]);
+						}
+						libera_lista(buffer);
+						buffer = cria_lista();
+						printf("Elementos transferidos para o bloco.\n\n");
+						cont=0;
+					}	
+				}
+				if(x==0){
+					printf("Erro!\n\n");
+				}else{
+					printf("Ficha inserida com sucesso!\n\n");
+				}
+			break;
+			case 5:
+				x = remove_final(buffer);
+				if(x==0){
+					printf("Erro!\n\n");
+				}else{
+					printf("Ficha removida com sucesso!\n\n");
+				}
+			break;
+			case 6:
+				printf("1.Verificar tamanho do buffer.\n");
+				printf("2.Verificar tamanho dos blocos.\n");
+				printf("Decisao: ");
+				scanf("%d",&aux);
+				system(CLEAR);
+				if(aux==1){
+					x = tamanho_lista(buffer);
+					if(x==-1){
+						printf("Erro!\n\n");
+					}else{
+						printf("A quantidade de jogadores no buffer eh:%d\n\n",x);
+					}
+				}else{
+					x = tamanho_lista(li);
+					if(x==-1){
+						printf("Erro!\n\n");
+					}else{
+						printf("A quantidade de jogadores nos blocos eh:%d\n\n",x);
+					}
+				}
+			break;
+			case 7:
+				printf("1.Imprimir o buffer.\n");
+				printf("2.Imprimir blocos.\n");
+				printf("Decisao: ");
+				scanf("%d",&aux);
+				system(CLEAR);
+				if(aux==1){
+					imprime(buffer, aux);
+				}else{
+					imprime(li, aux);
+				}
 			break;
 		}
-	}while(aux!=10);
+	}while(aux!=8);
 
 	return 0;
 }
