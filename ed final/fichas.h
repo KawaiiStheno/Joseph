92cdf57add44f0b9a462137aa509493ec1553f10
KawaiiStheno <47184562+KawaiiStheno@users.noreply.github.com>
2019-07-07 10:57:42 -0300

    Update fichas.h

--- ed final/fichas.h
+++ ed final/fichas.h
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#define CLEAR "cls"
+#else
+#define CLEAR "clear"
+#endif
+#define MAX 10
 
 struct entrada{
 	char nome[100], player[100], raca[100];
 	int fo, sab, inte, car, des, cons, id;
+	int hash, prevHash, nonce;
 };
 typedef struct elemento* Lista;
 
@@ -21,13 +29,15 @@ int tamanho_lista(Lista* li);
 
 int lista_vazia(Lista* li);
 
-int insere_ordenada(Lista* li, struct entrada al);
-
-int remove_inicio(Lista* li);
-
 int remove_final(Lista* li);
 
-void imprime(Lista* li);
+void imprime(Lista* li, int auxi);
 
-int remove_qlqr(Lista* li, int id);
+int hash(char data[]);
+
+int mine_block(char data[]);
+
+int recebe(struct entrada *al,int cont, int flag);
+
+int insere_final(Lista* li, struct entrada al);
 
